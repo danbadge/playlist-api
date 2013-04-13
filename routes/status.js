acf7e290babb9e7608b48dec486c6c96455d4815
Daniel <danbadge@gmail.com>
2013-04-13 20:27:45 +0100

    first commit - api with a single status endpoint

---
+++ routes/status.js
@@ -0,0 +1,4 @@
+exports.status = function (request, response) {
+    //response.set('text/plain');
+    response.send(200, 'ok');
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/status', function (req, res) {
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send('OK');
+});
+
+app.listen(process.env.PORT || 5000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
