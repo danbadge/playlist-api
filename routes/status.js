165653321807afa3e7ae0f03da38760e7cf817b4
Daniel <danbadge@gmail.com>
2013-04-13 20:31:47 +0100

    fix status response

--- routes/status.js
+++ routes/status.js
@@ -1,4 +1,4 @@
 exports.status = function (request, response) {
-    //response.set('text/plain');
-    response.send(200, 'ok');
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send('OK');
 };
